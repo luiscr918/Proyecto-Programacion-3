547645d1708de20f89f0928906926698ae2648a8
luiscr918 <lc9107040@gmail.com>
2025-06-18 20:14:21 -0500

    anotacion que hacia falta en admin repository

--- src/main/java/com/proyectoProgramacion3/repository/AdminRepository.java
+++ src/main/java/com/proyectoProgramacion3/repository/AdminRepository.java
@@ -2,6 +2,8 @@ package com.proyectoProgramacion3.repository;
 
 import com.proyectoProgramacion3.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin,Long> {
 }
